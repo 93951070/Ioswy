1af6983be876d0f5cdf872acfa570c168a2d75a8
93951070 <93951070@users.noreply.github.com>
2026-09-02 21:09:01 +0000

    fix: downloadDir 补 ExperimentalForeignApi opt-in

--- shared/src/iosMain/kotlin/me/wcy/music/shared/player/Downloader.ios.kt
+++ shared/src/iosMain/kotlin/me/wcy/music/shared/player/Downloader.ios.kt
@@ -1,8 +1,10 @@
 package me.wcy.music.shared.player
 
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSHomeDirectory
 
+@OptIn(ExperimentalForeignApi::class)
 actual fun downloadDir(): String {
     // Documents/Music 在本地音乐扫描（Documents 递归扫描）覆盖范围内，下载完成即可在「本地音乐」播到
     val dirPath = NSHomeDirectory() + "/Documents/Music"
